b57626c813f6e3d2995cbcbf0b18e4a0ca6a8206
pabloedu01 <pablo@pabloedu.com>
2025-09-30 10:16:23 -0300

    .

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with  turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': {os.getenv('DB_NAME')},
-        'USER': {os.getenv('DB_USER')},
-        'PASSWORD': {os.getenv('DB_PASSWORD')},
-        'HOST': {os.getenv('DB_HOST')},
-        'PORT': {os.getenv('DB_PORT')},
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
