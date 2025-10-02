2e9f88ed72367ce53d260bfeb2e5e73ae5c650e1
pabloedu01 <pablo@pabloedu.com>
2025-10-02 17:46:11 -0300

    Initial

---
+++ api/index.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'About'
+
+if __name__ == '__main__':
+    app.run(debug=True)
